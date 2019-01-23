8d3fc7b48796e8a02822d639da4929f1a47dd837
Cristian Lucero and Leilani Leach <cristian@>
2019-01-23 11:09:29 -0800

    add form that displays based on ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,17 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Begin brainstorming possible JavaScript group projects', 1),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  editTask() {
-    alert("Time to edit a task");
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
   priorityColor(currentTask) {
     if (currentTask.priority === 3) {
